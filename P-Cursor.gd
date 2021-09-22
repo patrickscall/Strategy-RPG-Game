506fe003d549993efc610722c3e5f4ed3de4e43a
Lazyninja1017 <66565039+Lazyninja1017@users.noreply.github.com>
2021-09-21 20:45:00 -0400

    -limited movement -can't overlap movements -greyed out cursor when beyond movement range

--- P-Cursor.gd
+++ P-Cursor.gd
@@ -1,6 +1,3 @@
-# Mum's the word
-
-
 extends Area2D
 var gridSize : Vector2 = Vector2(64,64)
 var viewPortSize : Vector2
@@ -8,78 +5,109 @@ var inputDirection : Vector2
 var Speed : int
 var disTraveled : int
 
+enum MoveDir {Up,Down,Left,Right}
+var CurrentMoveDir = MoveDir.Up
+var StartPos : Vector2
+var CurrentPos : Vector2
+
+
+var CanPlace : bool = true
 var selected
 var hovered
 export(NodePath) var SpritePath
 onready var CursorSprite = get_node(SpritePath)
 
+var LastColor : Color
 
-export (NodePath) var UnselectedMenuPath
-onready var UnselectedMenu = get_node(UnselectedMenuPath)
+
 
 
 func _process(_delta):
-	viewPortSize = get_viewport().size
+	print(LastColor)
+	# cursor movement
 	if Input.is_action_just_pressed("ui_down"): 
 		self.position.y += 64
+		CurrentMoveDir = MoveDir.Down
 	if Input.is_action_just_pressed("ui_up"):
 		self.position.y -= 64
+		CurrentMoveDir = MoveDir.Up
 	if Input.is_action_just_pressed("ui_right"):
 		self.position.x += 64
+		CurrentMoveDir = MoveDir.Right
 	if Input.is_action_just_pressed("ui_left"):
 		self.position.x -= 64
-		
+		CurrentMoveDir = MoveDir.Left
+	
+	# lock cursor to viewport
+	viewPortSize = get_viewport().size
 	if self.position.y >= viewPortSize.y:
 		self.position.y = viewPortSize.y - 64
+		CurrentMoveDir = MoveDir.Up
 	if self.position.y < 0:
 		self.position.y = 0
+		CurrentMoveDir = MoveDir.Down
 	if self.position.x >= viewPortSize.x:
 		self.position.x = viewPortSize.x - 64
+		CurrentMoveDir = MoveDir.Left
 	if self.position.x < 0:
 		self.position.x = 0
+		CurrentMoveDir = MoveDir.Right
 	
+	# selecting and moving units
 	if Input.is_action_just_pressed("ui_accept"):
-		if hovered != null and selected == null:
+		if hovered and !selected:
 			print(hovered)
 			selected = hovered
-			CursorSprite.set_modulate(Color(0.25, 0.7, 1))
+			change_sprite_color(Color(0.25, 0.7, 1))
 			selected.set_selected(true)
-		elif selected != null:
-			if selected == hovered:
-				CursorSprite.set_modulate(Color(1, 1, 1))
-				selected.set_selected(false)
-				selected = null
-			else:
+			StartPos = self.get_global_position()
+		elif selected and CanPlace: #placing unit on same space
 				selected._set_target_pos(self.get_global_position())
 				selected.move()
 				selected.set_selected(false)
 				selected = null
-				CursorSprite.set_modulate(Color(1, 1, 1))
+				change_sprite_color(Color(1, 1, 1))
+	if selected:
+		limit_movement(selected.MovementRange)
 		
 
-#func moveInput():
-#	inputDirection = Vector2.ZERO
-#	if Input.is_action_just_pressed("ui_left"):
-#		inputDirection.x = -1
-#	if Input.is_action_just_pressed("ui_up"):
-#		inputDirection.y = -1
-#	if Input.is_action_just_pressed("ui_right"):
-#		inputDirection.x = 1
-#	if Input.is_action_just_pressed("ui_down"):
-#		inputDirection.y = 1
 
-
-#func gridMovement():
-#	pass
 
 func _ready():
 	$AnimationPlayer.play("Idle")
 
 
+func limit_movement(var moverange):
+	CurrentPos = self.get_global_position()
+	if (StartPos - CurrentPos).length() > 64 * moverange:
+		CanPlace = false
+		change_sprite_color(Color(0.5, 0.5, 0.5))
+	else:
+		if !hovered:
+			CanPlace = true
+		change_sprite_color(Color(0.25, 0.7, 1))
+
+func change_sprite_color(var color : Color):
+	LastColor = CursorSprite.get_modulate()
+	CursorSprite.set_modulate(color)
+
+
 
 func _on_PCursor_body_entered(body):
+	if selected:
+		CanPlace = false
+#		match CurrentMoveDir:
+#			MoveDir.Up:
+#				self.position.y -= 64
+#			MoveDir.Down:
+#				self.position.y += 64
+#			MoveDir.Right:
+#				self.position.x += 64
+#			MoveDir.Left:
+#				self.position.x -= 64
 	hovered = body
 
 
 func _on_PCursor_body_exited(_body):
 	hovered = null
+
--- TestUnit.gd
+++ TestUnit.gd
@@ -3,6 +3,8 @@ extends KinematicBody2D
 export(NodePath) var MoveTargetPath
 onready var MoveTarget = get_node(MoveTargetPath)
 
+export(int,10) var MovementRange = 5
+
 var Selected : bool
 
 
@@ -16,6 +18,13 @@ func set_selected(var is_selected :bool = false):
 
 func _set_target_pos(var newtargetpos : Vector2 = Vector2.ZERO):
 	MoveTarget.set_global_position(newtargetpos)
+
+func make_pathing(var newpoint : Vector2):
+	$Path2D.Curve2D.add_point(newpoint)
+
+func clear_pathing():
+	$Path2D.Curve2D.clear_points()
+
 
 func move():
 	self.set_global_position(MoveTarget.get_global_position())
